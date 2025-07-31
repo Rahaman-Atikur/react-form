fdf044e9dae9bf58c8888d38f5ab92326e01f2c4
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-31 23:10:07 +0100

    Phase one done: Simple Form Created

--- src/Components/SimpleReactForm/SimpleForm.jsx
+++ src/Components/SimpleReactForm/SimpleForm.jsx
@@ -4,12 +4,15 @@ const SimpleForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(e.target.name.value);
+        console.log(e.target.email.value);
         console.log("Form Submitted");
     }
     return (
         <div>
             <form onSubmit={handleSubmit} >
                 <input type="name" name='name' placeholder='Enter Your Name' />
+                <br />
+                <input type="email" name='email' placeholder='Enter your Email' />
                 <br />
                 <button>Submit</button>
             </form>
