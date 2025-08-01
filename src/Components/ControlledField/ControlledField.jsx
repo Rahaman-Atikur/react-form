c246a2a127189c0f84940fa99b2528d730465df9
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-08-01 13:54:51 +0100

    Phase 2.5 done: Controlled Component pass validation,messege showing

--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
-
 const ControlledField = () => {
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const handleSubmit = (e) => {
         e.preventDefault();
         // console.log("Form Submitted");
         console.log(e.target.value);
+    }
+    const handlePasswordOnChange = (e) => {
+        // console.log(e.target.value);
+        setPassword(e.target.value);
 
+        if (password.length < 6) {
+            setError("Password should be 6 Charecter");
+        }
+        else {
+            setError("");
+        }
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" name='name' placeholder='name' required /> <br />
-                <input type="text" name='password' onChange={handleSubmit} defaultValue={password} placeholder='password' required /><br />
+                <input type="text" name='password' onChange={handlePasswordOnChange} defaultValue={""} placeholder='password' required /><br />
                 <button type='submit'>Submit</button>
             </form>
+            <p>{error}</p>
         </div>
     );
 };
