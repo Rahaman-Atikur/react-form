08df5b8d23324c3d9d7c8279e0723746a5f1340c
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-08-02 18:21:09 +0100

    Phase Four Done: Custom Hooks and Components are done

--- src/Components/HookForm/HookForm.jsx
+++ src/Components/HookForm/HookForm.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import useInputField from '../../Hooks/useInputField';
 
 const HookForm = () => {
-    const handleSubmit = ()=>{
+    const [name, nameOnChange] = useInputField("");
+    const [email, emailOnChange] = useInputField("");
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Submit");
+        console.log("Submit", name, email);
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                
-
+                <input defaultValue={name} type="text" onChange={nameOnChange} />
+                <br />
+                <input type="submit" value="submit" />
+                <br />
+                <input type="email" onChange={emailOnChange} defaultValue={email}
+                />
             </form>
         </div>
     );
